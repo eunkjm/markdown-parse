aee368ced7d12844c2a218fff63bce5c4c2be8da
eunkjm <deu05009@gmail.com>
2022-01-20 14:42:16 -0800

    checks if it's actually a link
    using if statement to check the placement of !

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
             if (nextOpenBracket == -1) {
                 break;
             }
+            if (markdown.indexOf("!") == nextOpenBracket -1){
+                break;
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             System.out.println("nextCloseBracket: " + nextCloseBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
